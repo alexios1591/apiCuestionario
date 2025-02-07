6b0469bbcf7f71bed4e5248e8bd5bb92558c5df7
lalechelml <ramos.joshelin20@gmail.com>
2025-02-07 06:43:36 -0500

    Add ClienteFactory for generating fake Cliente model data

---
+++ database/factories/ClienteFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cliente>
+ */
+class ClienteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'NomClie' => $this->faker->firstName,
+            'AppClie' => $this->faker->lastName,
+            'ApmClie' => $this->faker->lastName,
+            'EmaClie' => $this->faker->unique()->safeEmail,
+            'DniClie' => $this->faker->unique()->numerify('########'),
+            'FnaClie' => $this->faker->date('Y-m-d', '2003-12-31'),
+            'CelClie' => $this->faker->phoneNumber,
+            'localidad' => $this->faker->state,
+            'RegClie' => $this->faker->dateTime
+        ];
+    }
+}
